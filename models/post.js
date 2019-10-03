d32e14002c7e3bce9d9fa347094be808e56fda9a
Kim C <kimkly.chau@gmail.com>
2019-10-03 10:59:12 -0700

    created sequelize file stucture

---
+++ models/post.js
@@ -0,0 +1,55 @@
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+      product: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      street: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      },
+      appartment: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        len: [1]
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      zipcode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+    });
+  
+    Post.associate = function(models) {
+      // We're saying that a Post should belong to an User
+      // A Post can't be created without an User due to the foreign key constraint
+      Post.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Post;
+  };
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the secondtime_db if it exists currently --
+DROP DATABASE IF EXISTS secondtime_db;
+-- Creates the secondtime_db" database --
+CREATE DATABASE secondtime_db;
